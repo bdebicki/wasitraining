3e8aed76d432adc3200461f850e63eda3cc1df0f
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-08 21:59:04 +0200

    reverse order at timeline

--- src/js/actions/renderTimeline.js
+++ src/js/actions/renderTimeline.js
@@ -3,22 +3,27 @@
 import { LAYOUT, TIMELINE } from '../enums/classes';
 import { timelineEvent } from '../classes/timelineEvent';
 
+function sortEditions(list) {
+	return Object.keys(list).reverse();
+}
+
 export function renderTimeline(data, activeId) {
 	const timelineContainer = document.createElement('nav');
 	const editionsListContainer = document.createElement('ul');
+	const revertedEditionsOrder = () => sortEditions(data);
 
 	timelineContainer.id = TIMELINE.TIMELINE_ID;
 	editionsListContainer.classList.add(TIMELINE.EDITIONS_CLASS);
 
-	for(let editionId in data) {
-		let edition = new timelineEvent(data[editionId]);
+	revertedEditionsOrder().map((item) => {
+		let edition = new timelineEvent(data[item]);
 
 		if(edition.editionId == activeId) {
 			edition.render(editionsListContainer, true);
 		} else {
 			edition.render(editionsListContainer);
 		}
-	}
+	});
 
 	timelineContainer.appendChild(editionsListContainer);
 	document.getElementById(LAYOUT.TOP_ID).appendChild(timelineContainer);
