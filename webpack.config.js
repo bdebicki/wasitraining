5f5f30d296eddb2e7a55ff6438d62c26573784d6
Earl Debicki <bartosz.debicki@uxpin.com>
2017-08-20 15:09:13 +0200

    add build & watch scripts

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  entry: './src/js/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'wiraof.js',
+    publicPath: '/public'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.less$/i,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'less-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'wiraof.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/views/index.html'
+    }),
+  ]
+};
+
+module.exports = config;
