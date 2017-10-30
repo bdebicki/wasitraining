cd56a82424cf762a18230e212f745a88fe19197b
Earl Debicki <bartosz.debicki@uxpin.com>
2017-10-30 23:29:11 +0100

    move update edition actions to the year details

--- src/js/components/yearDetails/yearDetails.js
+++ src/js/components/yearDetails/yearDetails.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import { LAYOUT } from '../../enums/elementHandlers';
+import { LAYOUT, EDITION, RAIN } from '../../enums/elementHandlers';
+import { edition } from '../../classes/edition';
 import { editionDetails } from './editionDetails';
 import { rainDetails } from './rainDetails';
 
@@ -16,6 +17,21 @@ export class yearDetails {
 		section.id = LAYOUT.YEAR_CONTAINER_ID;
 
 		return section;
+	}
+
+	updateYearDetails() {
+		const editionId = this.editionId;
+		const newEdition = new edition(editionId);
+		const newEditionDetails = new editionDetails(editionId);
+		const newRainDetails = new rainDetails(editionId);
+
+		document.querySelector(`.${EDITION.YEAR_CLASS}`).textContent = newEdition.editionYear;
+		document.querySelector(`.${EDITION.DATES_CLASS}`).textContent = newEditionDetails.decorateEditionDates();
+		document.querySelector(`.${EDITION.FULL_NAME_CLASS}`).textContent = newEdition.editionFullName;
+		document.querySelector(`.${EDITION.PLACE_CLASS}`).textContent = newEdition.editionPlace;
+		document.querySelector(`.${EDITION.HEADLINERS_CLASS}`).appendChild(newEditionDetails.updateHeadliners());
+		document.querySelector(`.${RAIN.RAIN_CLASS}`).textContent = newEdition.editionRain;
+		document.getElementById(RAIN.EDITION_RAIN_DETAILS_ID).appendChild(newRainDetails.updateRainDetails());
 	}
 
 	render() {
--- src/js/views/yearView.js
+++ src/js/views/yearView.js
@@ -2,11 +2,8 @@
 
 import { LAYOUT, EDITION, RAIN } from '../enums/elementHandlers';
 import { VIEW_TYPES } from '../enums/viewTypes';
-import { edition } from '../classes/edition';
 import { header } from '../components/header/header';
 import { timeline } from '../components/timeline/timeline';
-import { editionDetails } from '../components/yearDetails/editionDetails';
-import { rainDetails } from '../components/yearDetails/rainDetails';
 import { yearDetails } from '../components/yearDetails/yearDetails'
 import { footer } from '../components/footer/footer';
 import { updateViewType } from '../utils/updateView';
@@ -31,17 +28,9 @@ export class yearView {
 
 	updateDetails() {
 		const newEditionId = this.data[this.editionId];
-		const newEdition = new edition(newEditionId);
-		const newEditionDetails = new editionDetails(newEditionId);
-		const newRainDetails = new rainDetails(newEditionId);
+		const yearBlock = new yearDetails(newEditionId);
 
-		document.querySelector(`.${EDITION.YEAR_CLASS}`).textContent = newEdition.editionYear;
-		document.querySelector(`.${EDITION.DATES_CLASS}`).textContent = newEditionDetails.decorateEditionDates();
-		document.querySelector(`.${EDITION.FULL_NAME_CLASS}`).textContent = newEdition.editionFullName;
-		document.querySelector(`.${EDITION.PLACE_CLASS}`).textContent = newEdition.editionPlace;
-		document.querySelector(`.${EDITION.HEADLINERS_CLASS}`).appendChild(newEditionDetails.updateHeadliners());
-		document.querySelector(`.${RAIN.RAIN_CLASS}`).textContent = newEdition.editionRain;
-		document.getElementById(RAIN.EDITION_RAIN_DETAILS_ID).appendChild(newRainDetails.updateRainDetails());
+		yearBlock.updateYearDetails();
 	}
 
 	render() {
