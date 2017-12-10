9c9d170b2e1590c2fc8b9070135310ff4ba04f8c
Earl Debicki <bartosz.debicki@uxpin.com>
2017-12-11 00:43:51 +0100

    split lineup settings into separated getters

--- src/js/classes/lineup.js
+++ src/js/classes/lineup.js
@@ -2,13 +2,24 @@
 
 export class lineup {
 	constructor(editionId) {
-		this._edition = editionId;
 		this._editionDetails = editionId.details;
 		this._settings = editionId.lineupSettings;
 	}
 
 	get settings() {
 		return this._settings;
+	}
+
+	get sortType() {
+		return this.settings.sortType;
+	}
+
+	get mergeFays() {
+		return this.settings.mergeDays;
+	}
+
+	get otherArtists() {
+		return this.settings.otherArtists;
 	}
 
 	get lineup() {
