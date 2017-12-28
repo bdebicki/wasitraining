afc8f0c1418c08476cbbed1dfc0fe71c46cd9604
Earl Debicki <bartosz.debicki@uxpin.com>
2017-12-28 22:36:33 +0100

    set yaer view as a default on load

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,20 @@
 'use strict';
 
 import { DATA_URL } from './enums/data';
-import { introView } from './views/introView'
-// import { yearView } from './views/yearView'
-
+// import { introView } from './views/introView'
+import { yearView } from './views/yearView'
 import '../less/app.less';
 
 function app() {
 	fetch(DATA_URL)
 		.then(response => response.json())
 		.then((data) => {
-			const intro = new introView(data);
-
-			intro.render();
-			// const year = new yearView(data, '16');
+			// const intro = new introView(data);
 			//
-			// year.render();
+			// intro.render();
+			const year = new yearView(data, '16');
+
+			year.render();
 		})
 		.catch((error) => {
 			console.log(error);
