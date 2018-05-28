ee500f7ba12ba7201f676ceb8adf5a101572d783
Earl Debicki <bartosz.debicki@uxpin.com>
2018-05-29 00:18:35 +0200

    rename getFestivalsDataList and make it reusable + add getDataFromFiles module + save data as an file with array

--- tools/builder-festivalData.js
+++ tools/builder-festivalData.js
@@ -1,3 +1,12 @@
-const getFestivalsDataList = require('./utils/getFestivalsDataList');
+const fs = require('fs');
+const path = require('path');
+const getFestivalsDataList = require('./utils/getFilesList');
+const getDataFromFiles = require('./utils/getDataFromFiles');
 
-getFestivalsDataList();
+const sourceFilesPath = path.join(__dirname, '../src/data/festivals');
+const dataFile = path.join(__dirname, '../public/festivalEditions.json');
+
+const festivalsDataList = getFestivalsDataList(sourceFilesPath);
+const dataFromFiles = getDataFromFiles(festivalsDataList);
+
+fs.writeFileSync(dataFile, JSON.stringify(dataFromFiles, null, '\t'), 'utf-8');
---
+++ tools/utils/getFilesList.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function getFilesList(filesPath) {
+	return fs.readdirSync(filesPath).map(
+		(fileName) => path.join(filesPath, fileName)
+	);
+};
