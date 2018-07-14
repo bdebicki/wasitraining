a3c7eed451bc475f4df037e50c5217ff609c30dd
Earl Debicki <bartosz.debicki@uxpin.com>
2018-07-14 16:06:13 +0200

    add styles for getAlignClassName

--- src/js/components/yearDetails/classNames/__tests__/getArtistLvlClassName.test.js
+++ src/js/components/yearDetails/classNames/__tests__/getArtistLvlClassName.test.js
@@ -1,9 +1,10 @@
 import getAlignClassName from '../getArtistLvlClassName';
 
-describe('getAlignClassName', () => {
+describe('test return value from getAlignClassName function', () => {
 	it('return headliner class name', () => {
 		expect(getAlignClassName('headliners')).toBe('lineupArtists__artist--headliner');
 	});
+
 	it('return lvl class name', () => {
 		expect(getAlignClassName('lvl1')).toBe('lineupArtists__artist--lvl1');
 	});
