364a3f0376cd837414486acde5d9a7ac4b6b69e3
Earl Debicki <bartosz.debicki@uxpin.com>
2018-07-22 23:15:18 +0200

    add snapshot test for cookies info

---
+++ src/js/components/footer/__tests__/CookiesInfo.test.js
@@ -0,0 +1,9 @@
+import CookiesInfo from '../CookiesInfo';
+
+describe('test cookies info section', () => {
+	it('render cookies section', () => {
+		const cookies = CookiesInfo.render();
+
+		expect(cookies).toMatchSnapshot();
+	});
+});
