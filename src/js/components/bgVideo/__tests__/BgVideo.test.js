b23bd082dd0ca9af98719ab6dbbbe688149eae73
Earl Debicki <bartosz.debicki@uxpin.com>
2018-07-27 00:59:48 +0200

    fix tests after move append node to util

--- src/js/components/bgVideo/__tests__/BgVideo.test.js
+++ src/js/components/bgVideo/__tests__/BgVideo.test.js
@@ -1,23 +1,11 @@
-import cleanDOM from '../../../../../testUtils/cleanDOM';
-import prepareContainer from '../../../../../testUtils/prepareContainer';
 import BgVideo from '../BgVideo';
-import BG from '../elementHandlers/background';
-
-const videoContainerId = 'videoContainer';
-
-afterAll(() => cleanDOM());
 
 describe('test background video component', () => {
-	prepareContainer(videoContainerId);
-
-	it('render background video into container', () => {
-		// having
-		const videoEl = new BgVideo(videoContainerId);
-
+	it('render background video component', () => {
 		// when
-		videoEl.render();
+		const videoEl = BgVideo.render();
 
 		// then
-		expect(document.querySelector(`.${BG.VIDEO_CLASS}`)).toBeTruthy();
+		expect(videoEl).toMatchSnapshot();
 	});
 });
