4dfa8c3139aaac4ad52e3d06e906792aa8fc068d
Earl Debicki <bartosz.debicki@uxpin.com>
2018-11-21 00:40:48 +0100

    implement addSvgElement to setIcon

--- src/js/utils/setIcon.js
+++ src/js/utils/setIcon.js
@@ -1,18 +1,13 @@
+import addSvgElement from './addSvgElement';
+
 export default function setIcon(shape, classNames) {
-	const xmlns = 'http://www.w3.org/2000/svg';
-	const svg = document.createElementNS(xmlns, 'svg');
-
-	svg.setAttributeNS(null, 'width', `${shape.width}px`);
-	svg.setAttributeNS(null, 'height', `${shape.height}px`);
-	svg.setAttributeNS(null, 'viewBox', `0 0 ${shape.width} ${shape.height}`);
-	svg.classList.add(shape.className);
-	svg.appendChild(shape.icon);
-
-	if (classNames && typeof classNames === 'string') {
-		svg.classList.add(classNames);
-	} else if (classNames && Array.isArray(classNames)) {
-		classNames.forEach((className) => svg.classList.add(className));
-	}
-
-	return svg;
+	return addSvgElement('svg', {
+		attrs: [
+			{ width: `${shape.width}px` },
+			{ height: `${shape.height}px` },
+			{ viewBox: `0 0 ${shape.width} ${shape.height}` },
+		],
+		children: shape.icon,
+		classNames: [shape.className].concat(classNames),
+	});
 }
