d53f15efc5c7cf9cf7b3f5c70fe9355b43917ce1
Earl Debicki <bartosz.debicki@uxpin.com>
2018-11-21 00:40:25 +0100

    add addSvgElement function

---
+++ src/js/utils/addSvgElement.js
@@ -0,0 +1,34 @@
+import setChildren from './setChildren';
+import setClassName from './setClassName';
+
+const svgType = 'http://www.w3.org/2000/svg';
+
+export default function(tag, {
+	attrs,
+	children,
+	classNames,
+	id,
+} = {}) {
+	const svgElement = document.createElementNS(svgType, tag);
+	const setAttr = (attr) => {
+		const attrName = Object.keys(attr)[0];
+		svgElement.setAttributeNS(null, attrName, attr[attrName]);
+	};
+
+	setChildren(svgElement, children);
+	setClassName(svgElement, classNames);
+
+	if (id) {
+		svgElement.id = id;
+	}
+
+	if (typeof (attrs) === 'object' && !Array.isArray(attrs)) {
+		setAttr(attrs);
+	} else if (Array.isArray(attrs)) {
+		attrs.forEach((attr) => {
+			setAttr(attr);
+		});
+	}
+
+	return svgElement;
+}
