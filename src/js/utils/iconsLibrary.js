558460d1678a2687808572b85f07184581559e6a
Earl Debicki <bartosz.debicki@uxpin.com>
2018-11-22 20:48:46 +0100

    implement addSvgElement in licons library

--- src/js/utils/iconsLibrary.js
+++ src/js/utils/iconsLibrary.js
@@ -1,24 +1,28 @@
+import addSvgElement from './addSvgElement';
 import { ICONS } from './elementHandlers/icons';
-
-const xmlns = 'http://www.w3.org/2000/svg';
 
 const icons = {
 	plus: () => {
 		const icon = document.createDocumentFragment();
-		const rectH = document.createElementNS(xmlns, 'rect');
-		const rectV = document.createElementNS(xmlns, 'rect');
 		const width = 5;
 		const height = 5;
 		const className = ICONS.PLUS;
-
-		rectH.setAttributeNS(null, 'x', '0');
-		rectH.setAttributeNS(null, 'y', '2');
-		rectH.setAttributeNS(null, 'width', '5');
-		rectH.setAttributeNS(null, 'height', '1');
-		rectV.setAttributeNS(null, 'x', '2');
-		rectV.setAttributeNS(null, 'y', '0');
-		rectV.setAttributeNS(null, 'width', '1');
-		rectV.setAttributeNS(null, 'height', '5');
+		const rectH = addSvgElement('rect', {
+			attrs: [
+				{ x: '0' },
+				{ y: '2' },
+				{ width: '5' },
+				{ height: '1' },
+			],
+		});
+		const rectV = addSvgElement('rect', {
+			attrs: [
+				{ x: '2' },
+				{ y: '0' },
+				{ width: '1' },
+				{ height: '5' },
+			],
+		});
 
 		icon.appendChild(rectH);
 		icon.appendChild(rectV);
@@ -30,22 +34,27 @@ const icons = {
 
 	close: () => {
 		const icon = document.createDocumentFragment();
-		const rectH = document.createElementNS(xmlns, 'rect');
-		const rectV = document.createElementNS(xmlns, 'rect');
 		const width = 10;
 		const height = 10;
 		const className = ICONS.CLOSE;
-
-		rectH.classList.add(ICONS.CLOSE_RECT);
-		rectH.setAttributeNS(null, 'x', '-1');
-		rectH.setAttributeNS(null, 'y', '4');
-		rectH.setAttributeNS(null, 'width', '12');
-		rectH.setAttributeNS(null, 'height', '2');
-		rectV.classList.add(ICONS.CLOSE_RECT);
-		rectV.setAttributeNS(null, 'x', '4');
-		rectV.setAttributeNS(null, 'y', '-1');
-		rectV.setAttributeNS(null, 'width', '2');
-		rectV.setAttributeNS(null, 'height', '12');
+		const rectH = addSvgElement('rect', {
+			classNames: ICONS.CLOSE_RECT,
+			attrs: [
+				{ x: '-1' },
+				{ y: '4' },
+				{ width: '12' },
+				{ height: '2' },
+			],
+		});
+		const rectV = addSvgElement('rect', {
+			classNames: ICONS.CLOSE_RECT,
+			attrs: [
+				{ x: '4' },
+				{ y: '-1' },
+				{ width: '2' },
+				{ height: '12' },
+			],
+		});
 
 		icon.appendChild(rectH);
 		icon.appendChild(rectV);
