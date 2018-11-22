196b537fe755f5a59f63fe1471d664a2cb2325fe
Earl Debicki <bartosz.debicki@uxpin.com>
2018-11-22 21:58:40 +0100

    use addSvgElement in addSvgMask

--- src/js/utils/addSvgMask.js
+++ src/js/utils/addSvgMask.js
@@ -1,3 +1,4 @@
+import addSvgElement from './addSvgElement';
 import SVG_MASK from './elementHandlers/svg';
 
 export const svgType = 'http://www.w3.org/2000/svg';
@@ -8,30 +9,32 @@ export function addSVGmask({
 	maskShape,
 	svgClass,
 }) {
-	const svg = document.createElementNS(svgType, 'svg');
-	const defs = document.createElementNS(svgType, 'defs');
-	const mask = document.createElementNS(svgType, 'mask');
-	const rectMask = document.createElementNS(svgType, 'rect');
-	const rectBg = document.createElementNS(svgType, 'rect');
+	const rectMask = addSvgElement('rect', { classNames: SVG_MASK.MASK_BASE_CLASS });
+	const rectBg = addSvgElement('rect', {
+		classNames: [
+			SVG_MASK.MASK_BG_CLASS,
+			maskBgClass,
+		],
+		properties: { mask: `url(#${maskId})` },
+	});
+	const mask = addSvgElement('mask', {
+		children: [
+			rectMask,
+			maskShape,
+		],
+		id: maskId,
+		properties: { maskUnits: 'userSpaceOnUse' },
+	});
+	const defs = addSvgElement('defs', { children: mask });
 
-	svg.classList.add(SVG_MASK.MASK_CLASS);
-	if (svgClass) {
-		svg.classList.add(svgClass);
-	}
-	mask.id = maskId;
-	mask.setAttributeNS(null, 'maskUnits', 'userSpaceOnUse');
-	rectMask.classList.add(SVG_MASK.MASK_BASE_CLASS);
-	rectBg.classList.add(SVG_MASK.MASK_BG_CLASS);
-	if (maskBgClass) {
-		rectBg.classList.add(maskBgClass);
-	}
-	rectBg.setAttributeNS(null, 'mask', `url(#${maskId})`);
-
-	mask.appendChild(rectMask);
-	mask.appendChild(maskShape);
-	defs.appendChild(mask);
-	svg.appendChild(defs);
-	svg.appendChild(rectBg);
-
-	return svg;
+	return addSvgElement('svg', {
+		children: [
+			defs,
+			rectBg,
+		],
+		classNames: [
+			SVG_MASK.MASK_CLASS,
+			svgClass,
+		],
+	});
 }
