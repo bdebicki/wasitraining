e04cc28cb31adb842094c43b1408d8cb5eebdca8
Earl Debicki <bartosz.debicki@uxpin.com>
2019-01-04 23:45:26 +0100

    add clean addSvgMask file

--- src/js/utils/addSvgMask.js
+++ src/js/utils/addSvgMask.js
@@ -2,20 +2,11 @@ import addSvgElement from './addSvgElement';
 import SVG_MASK from './elementHandlers/svg';
 
 export default function({
-	maskBgClass,
 	maskId,
 	maskShape,
-	svgClass,
 }) {
 	const rectMaskSettings = {
 		classNames: SVG_MASK.MASK_BASE_CLASS,
-	};
-	const bgSettings = {
-		classNames: [
-			SVG_MASK.MASK_BG_CLASS,
-			maskBgClass,
-		],
-		properties: { mask: `url(#${maskId})` },
 	};
 	const maskSettings = {
 		children: [
@@ -25,20 +16,6 @@ export default function({
 		id: maskId,
 		properties: { maskUnits: 'userSpaceOnUse' },
 	};
-	const defsSettings = {
-		children: addSvgElement('mask', maskSettings),
-	};
-	const svgSettings = {
-		children: [
-			addSvgElement('defs', defsSettings),
-			addSvgElement('rect', bgSettings),
-		],
-		classNames: [
-			SVG_MASK.MASK_CLASS,
-			svgClass,
-		],
-	};
 
-	// return addSvgElement('svg', svgSettings);
 	return addSvgElement('mask', maskSettings);
 }
