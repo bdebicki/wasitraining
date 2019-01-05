1480dd846b3f16cb6cf172ac5335fb1715d3d6ab
Earl Debicki <bartosz.debicki@uxpin.com>
2019-01-05 12:15:55 +0100

    handle styles for rain masks

--- src/js/components/background/elementHandlers/background.js
+++ src/js/components/background/elementHandlers/background.js
@@ -6,6 +6,10 @@ const bgClassBuilders = {
 };
 const BG = {
 	COVER_CLASS: `${bgClassBuilders.cover}__cover`,
+	COVER_SHAPE_CLASS: `${bgClassBuilders.cover}__coverShape`,
+	MASK_SHAPE: `${bgClassBuilders.cover}__maskShape`,
+	MASK_SHAPE_YES: `${bgClassBuilders.cover}__maskShape--yes`,
+	MASK_SHAPE_NO: `${bgClassBuilders.cover}__maskShape--no`,
 	VIDEO_CLASS: `${bgClassBuilders.video}__video`,
 };
 
