46af532b3190630df5ea8515d922e16ba49e94ed
Earl Debicki <bartosz.debicki@uxpin.com>
2019-02-17 22:28:05 +0100

    optimize timeline scrolling + move variables to constructor &  validate mouse events

--- src/js/components/timeline/TimelineScrolling.js
+++ src/js/components/timeline/TimelineScrolling.js
@@ -7,6 +7,10 @@ export default class TimelineScrolling {
 		this.areaCoords = this.getAreaCoords();
 		this.areaSize = this.getAreaSize();
 		this.editionsSize = TimelineScrolling.getEditionsSize();
+		this.editionsMinScroll = 0;
+		this.editionsMaxScroll = this.editionsSize - this.areaSize;
+		this.editionsCurrentScroll = null;
+		this.cursorPosition = null;
 		this.handleScrolling = this.handleScrolling.bind(this);
 	}
 
@@ -16,19 +20,54 @@ export default class TimelineScrolling {
 
 	handleScrolling(e) {
 		const editionListEl = document.querySelector(`.${TIMELINE.MAIN_EDITIONS_CLASS}`);
-		const { x1, x2 } = this.areaCoords;
-		const cursorPosition = TimelineScrolling.getCursorPosition(e);
 
-		if (cursorPosition >= x1 && cursorPosition <= x2) {
-			editionListEl.style.transform = `translateX(-${this.setTimelineScroll(e)}px)`;
+		this.handleCursorMove(e);
+		this.setTimelineScroll();
+		editionListEl.style.transform = `translateX(-${this.editionsCurrentScroll}px)`;
+	}
+
+	handleCursorMove(e) {
+		this.setCursorPosition(e);
+	}
+
+	setTimelineScroll() {
+		const {
+			areaCoords,
+			areaSize,
+			cursorPosition,
+			editionsMinScroll,
+			editionsMaxScroll,
+			editionsSize,
+		} = this;
+		const { x1, x2 } = areaCoords;
+		const editionsScroll = Math.round(((editionsSize - areaSize) * (cursorPosition - x1)) / areaSize);
+
+		if (cursorPosition <= x1) { // while cursor is before beginning
+			this.editionsCurrentScroll = editionsMinScroll;
+		} else if (cursorPosition >= x2) { // while cursor is after timeline
+			this.editionsCurrentScroll = editionsMaxScroll;
+		} else {
+			this.editionsCurrentScroll = editionsScroll;
 		}
 	}
 
-	setTimelineScroll(e) {
-		const { areaSize, editionsSize } = this;
-		const cursorPosition = TimelineScrolling.getCursorPosition(e) - this.areaCoords.x1;
+	setCursorPosition(e) {
+		const newCursorPosition = TimelineScrolling.getCursorPosition(e);
+		const { x1, x2 } = this.areaCoords;
+		const {
+			editionsCurrentScroll,
+			editionsMinScroll,
+			editionsMaxScroll,
+		} = this;
 
-		return Math.round(((editionsSize - areaSize) * cursorPosition) / areaSize);
+		if (this.cursorPosition === newCursorPosition
+			|| (newCursorPosition <= x1 && editionsCurrentScroll === editionsMinScroll)
+			|| (newCursorPosition >= x2 && editionsCurrentScroll === editionsMaxScroll)
+		) {
+			return;
+		}
+
+		this.cursorPosition = newCursorPosition;
 	}
 
 	static getCursorPosition(e) {
